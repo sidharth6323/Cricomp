a46a7325242fc59fec0f51f37d84279ad4494795
unknown <vsetia46@gmail.com>
2016-02-04 23:40:15 +0530

    cricomp commit2

---
+++ Cricomp/static/js/bscscripts.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+document.getElementById("signup-trigger-homepage").onclick=function(){
+	document.getElementById("signup-form-screen").style.display="block";
+};
+
+document.getElementById("close-signup-form").onclick=function(){
+	document.getElementById("signup-form-screen").style.display="none";
+};
