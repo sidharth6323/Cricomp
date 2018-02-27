0be2c94cbb0c4bb4dfb40f950e37c6ee042b8ed6
SIDHARTH KUMAR <sidharth6323@gmail.com>
2018-02-28 01:45:58 +0530

    deleted old stuff

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\sidharth\desktop\projects\cricomp\cricompenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
