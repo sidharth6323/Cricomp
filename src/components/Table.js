7e866532a065ae21ffccd6905a0407caa11b41a1
SIDHARTH KUMAR <sidharth6323@gmail.com>
2018-02-28 02:40:37 +0530

    added loader and other optimization

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,17 +8,18 @@ class Table extends React.Component
   constructor(){
     super();
     var ws = new WebSocket("ws://stocks.mnet.website"); //opening the WebSocket connection
-    this.state={};
+    this.state={spinner:true};
     ws.onopen = ()=>{console.log("connection is open");}; //handle open event of WebSocket
     ws.onmessage = (evt)=>{this.handleMessage(evt)};  //handle message event of WebSocket
     window.onbeforeunload = (event)=>{ws.close()};  //close the connection of WebSocket
   }
   //function to update stock data
   handleMessage(evt){
+      this.setState({spinner:false});
       var received_msg = JSON.parse(evt.data);
       received_msg.forEach(([name, price]) => {
         let tempobj={};
-        var className="";
+        var className="white";
         if(this.state[name])
          className = this.state[name]["price"]>price?'red':'green';
         tempobj[name]={"price":price,"time":new Date(),"className":className}
@@ -29,8 +30,14 @@ class Table extends React.Component
   render(){
     return(
       <div className="table">
-        <table>
-          <thead>
+        <div className={'spinner-container ' + (this.state.spinner ? 'visible' : 'hidden')  }>
+          <div class="half-circle-spinner">
+            <div class="circle circle-1"></div>
+            <div class="circle circle-2"></div>
+          </div>
+        </div>
+        <table className={this.state.spinner ? 'hidden' : 'visible'}>
+          <thead style={{ display: this.state.spinner ? 'none' : 'table-header-group' }}>
             <tr>
               <th>Ticker</th>
               <th>Price</th>
@@ -39,6 +46,7 @@ class Table extends React.Component
           </thead>
           <tbody>
             {Object.keys(this.state).map(function(key) {
+                if(key!='spinner')
                 return <tr key={key}>
                           <td className="ticker"><span className={this.state[key]['className']}></span>{key}</td>
                           <td>$ {this.state[key]["price"].toFixed(3)}</td>
